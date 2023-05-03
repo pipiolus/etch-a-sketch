c5290af302374634779e5fba60d6c69b25eefd97
pipiolus <giovanipaolonii@gmail.com>
2023-05-03 10:44:53 -0300

    Project initialized and working

---
+++ app.js
@@ -0,0 +1,54 @@
+const grid = document.querySelector('#container');
+const input = document.getElementById('quantity')
+const reset = document.querySelector('.reset')
+
+
+
+const createGrid = () => {
+   for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    grid.appendChild(div);
+   } 
+};
+
+const updateGrid = () => {
+    if(input.value > 100) {
+        window.alert("THE SIZE MUST BE LESS THAN 100")
+        return;
+    }
+
+    grid.innerHTML = "";
+    grid.style.setProperty(
+        "grid-template-columns",
+        `repeat(${input.value}, 2fr)`
+    );
+    grid.style.setProperty(
+        "grid-template-rows",
+        `repeat(${input.value}, 2fr)`
+    );
+    for (let i = 0; i < input.value * input.value; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        grid.appendChild(div);
+    }
+};
+
+
+const square = document.querySelector('div');
+square.addEventListener('mouseover', (e) => {
+    e.target.classList.replace('square', 'color');
+})
+
+input.addEventListener('change', updateGrid);
+
+reset.addEventListener('click', function() {
+    grid.innerHTML = "";
+    input.value = "";
+    grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    createGrid();
+    
+});
+
+createGrid();
